8df8ca777776b0ef599882998da045531c2d7f47
Alessandro Crud <alecrud@gmail.com>
2017-03-31 01:30:01 -0300

    Alterado para ter o atributo email. Método toString() sobrescrito.

--- nichos/src/com/crud/nichos/model/Usuario.java
+++ nichos/src/com/crud/nichos/model/Usuario.java
@@ -3,13 +3,14 @@ package com.crud.nichos.model;
 public class Usuario {
 
 	private String name;
+	private String email;
 
 	public Usuario() {
 	}
 
-	public Usuario(String name) {
-		super();
+	public Usuario(String name, String email) {
 		this.name = name;
+		this.setEmail(email);
 	}
 
 	public String getName() {
@@ -20,4 +21,16 @@ public class Usuario {
 		this.name = name;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
 }
