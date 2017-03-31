be4f35ad681e8ea9d70da974c00dc6c9498d9a12
Alessandro Crud <alecrud@gmail.com>
2017-03-31 00:51:34 -0300

    Primeiro Commit da aplicação Nichos utilizando o EGit do Eclipse.

---
+++ nichos/src/com/crud/nichos/model/Usuario.java
@@ -0,0 +1,23 @@
+package com.crud.nichos.model;
+
+public class Usuario {
+
+	private String name;
+
+	public Usuario() {
+	}
+
+	public Usuario(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
